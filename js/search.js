db54cb86ff5f5036c67186b6260759219bbb09e9
zubidasubhi <zubida.subhi@griffithuni.edu.au>
2015-10-23 13:21:54 +1000

    login

--- js/search.js
+++ js/search.js
@@ -85,7 +85,7 @@ function getDesc(){
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
-      testAPI();
+      //testAPI();
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
       document.getElementById('status').innerHTML = 'Please log ' +
