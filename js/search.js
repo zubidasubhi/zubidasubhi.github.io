0456d9a817a9f0b36585be2ad871af0af8e7d5e6
zubidasubhi <zubida.subhi@griffithuni.edu.au>
2015-10-23 17:26:14 +1000

    tyftfhgyf

--- js/search.js
+++ js/search.js
@@ -3,7 +3,7 @@ var APIkey = "api_key=a1d7aeadeab7b9ed2ce660fff95dc200";
 //Sectret=
 //c915a2554ca01bbc186323c48505b494
 //DMS facebook page ID = 1493089840986763
-
+FB.init({appId:'1493089840986763', xfbml: true, version:'v2.5' });
 
 	
 $(document).ready(function () {
