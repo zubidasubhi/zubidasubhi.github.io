52456dae386614b9e517c4a1aa3a4bb7a0b62017
zubidasubhi <zubida.subhi@griffithuni.edu.au>
2015-10-24 12:56:50 +1000

    sd

--- js/search.js
+++ js/search.js
@@ -44,6 +44,14 @@ $(document).ready(function () {
 FB.getLoginStatus(function(response) {
    statusChangeCallback(response);
   });
+  
+    (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 
 
 
